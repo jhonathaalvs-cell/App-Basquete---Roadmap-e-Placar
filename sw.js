2fb144fa983dc59a00466eef75415d3e5fc97bdd
wanderson jhonatha <jhonathaalvs@gmail.com>
2026-03-21 16:15:16 -0300

    versão 0.0.5

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'basket-v1';
+const assets = ['./', './index.html', './style.css', './script.js'];
+
+// Instala e guarda os arquivos no cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Busca os arquivos no cache se estiver offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
